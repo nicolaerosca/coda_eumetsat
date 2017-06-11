256fc0bf5e8580c6f105ce22f1b1bcf93cb5935d
Nicolae Rosca <nicolae.rosca@xaxis.com>
2017-06-11 15:54:03 +0200

    created pip module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='coda_eumetsat',
+      version='0.1',
+      description='Copernicus EUMETSAT DataClient',
+      url='http://github.com/nicolaerosca/coda_eumetsat',
+      author='EUMETSAT GreenTeam',
+      author_email='nicolae.roshca@gmail.com',
+      license='MIT',
+      packages=['coda_eumetsat'],
+      zip_safe=False)
